400487a6b95e63e11bd9afede36fd8fc7abd90a4
Renesa khatun <krenesa55@gmail.com>
2020-06-28 22:53:21 +0600

    seperated contact about page

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('components.index');
+});
+Route::get('/about', function () {
+    return view('components.about');
+});
+Route::get('/team', function () {
+    return view('components.team');
+});
+
+Route::get('/contact', function () {
+    return view('components.contact');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
